16311e3ca08fceecc2ce482c978d3d32ca2c4886
Vamshi Krishna <vamshikrishnaj5@gmail.com>
2020-04-24 10:21:49 -0500

    Template reference Variable example

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -6,25 +6,65 @@ import { Component, OnInit } from '@angular/core';
   <h2> 
   Welcome {{name}} // this is called interpolation
   </h2>
+  <input #myInput type = "text">
+  <button (click) = "logMessage(myInput.value)" > Log </button>
   <h2>{{greetUser()}}</h2>
-  <input [id] = "myId" type = "text" value = "Vishwas">
-  <input [disabled] = "isDisabled" type = "text" value = "Vishwas">
+  <h2 class = "text-success">{{name}}</h2>
+  <h2 [class.text-danger] = "hasError">{{name}}</h2>
+  <h2 [style.color] = "hasError ? 'red' : 'green'">Style Binding </h2>
+  <input [id] = "myId" type = "text" value = "Vamshi">
+  <h2 [ngClass] = "messageClass" > Vamshi ng Class </h2>
+  <input [disabled] = "isDisabled" type = "text" value = "Vamshi">
+  <button (click) = "onClick($event)" > Greet </button>
+  <button (click) = "greeting = 'Welcome Vishwas'" > Greet1 </button>
   `,
-  styles: []
+  styles: [ `
+  
+  .text-success {
+    color : green
+  }
+
+  .text-danger {
+    color : red
+  }
+  .text-special {
+    font-style : italic
+  }
+  `]
 })
 export class TestComponent implements OnInit {
 
   public name = "Vamshi"
   public siteUrl = window.location.href
   public myId = "testID"
+  public successClass = "text-success";
   public isDisabled = true
+  public hasError = false
+  public isSpecial = true
+  public greeting = ""
+
+  public messageClass = {
+    "text-success" : !this.hasError,
+    "text-danger" : this.hasError,
+    "text-special" : this.isSpecial
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClick(event) {
+    console.log(event)
+    this.greeting = event.type;
+  }
+
   greetUser(){
     return "Hello "+this.name;
+  }
+
+  logMessage(value){
+     console.log(value);
   }
 
 }
